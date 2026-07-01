cc474bb5f258c96ccf3b95979d851ae1beeb0d89
AntoGuaro10 <antonio.guarisco96@gmail.com>
2026-07-01 11:50:05 +0200

    add some output values on ec2 module

---
+++ modules/ec2/output.tf
@@ -0,0 +1,19 @@
+output "instance_subnet_id" {
+  value = aws_instance.instance.subnet_id
+}
+
+output "instance_private_ip" {
+  value = aws_nework_interface.network_interface.private_ip
+}
+
+output "instance_public_ip" {
+  value = aws_instance.instance.public_ip
+}
+
+output "instance_zone" {
+  value = aws_instance.instance.availability_zone
+}
+
+output "instance_id" {
+  value = aws_instance.instance.id
+}
