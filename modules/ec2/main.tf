e8d2e4caf08309f9491245a256cd43878803c8b7
AntoGuaro10 <antonio.guarisco96@gmail.com>
2026-07-02 12:26:17 +0200

    refactor security group

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -48,18 +48,26 @@ resource "aws_network_interface" "network_interface" {
 #   device_index         = 0
 # }
 
-resource "aws_security_group" "ssh" {
-  name        = "ssh-access-sg"
-  description = "Allow SSH access to ec2 instances"
-  vpc_id      = data.aws_vpc.default_vpc.id
+resource "aws_vpc_security_group_ingress_rule" "allow_tcp_ipv4" {
+  security_group_id = aws_security_group.tcp.id
+  from_port         = 0
+  ip_protocol       = "tcp"
+  to_port           = 0
+  cidr_ipv4         = [var.ssh_cidr_blocks]
+}
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.ssh_cidr_blocks
-  }
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.tcp.id
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+  cidr_ipv4         = [var.ssh_cidr_blocks]
+}
+
+resource "aws_security_group" "tcp" {
+  name        = "internet-access-sg"
+  description = "Allow connection to ec2 instances"
+  vpc_id      = data.aws_vpc.default_vpc.id
 
   egress {
     from_port   = 0
@@ -86,7 +94,7 @@ resource "aws_instance" "instance" {
   availability_zone           = var.zone
   subnet_id                   = aws_network_interface.network_interface.subnet_id
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.ssh.id]
+  vpc_security_group_ids      = [aws_security_group.tcp.id]
 
   key_name = aws_key_pair.key_pair.key_name
 
